73891fd21eeacac67dbaa808c0526d742dea1496
pierrelambert41 <pierrelambert41@gmail.com>
2019-03-30 20:50:39 +0100

    ajout methode & css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {TodoDataService} from './todo-data.service';
+import { Todo } from './todo';
 
 @Component({
   selector: 'app-root',
@@ -7,14 +8,25 @@ import {TodoDataService} from './todo-data.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  // Ask Angular DI system to inject the dependency
-  // associated with the dependency injection token `TodoDataService`
-  // and assign it to a property called `todoDataService`
+  newTodo: Todo = new Todo();
+
   constructor(private todoDataService: TodoDataService) {
   }
 
-  // Service is now available as this.todoDataService
+  addTodo() {
+    this.todoDataService.addTodo(this.newTodo);
+    this.newTodo = new Todo();
+  }
+
   toggleTodoComplete(todo) {
     this.todoDataService.toggleTodoComplete(todo);
+  }
+
+  removeTodo(todo) {
+    this.todoDataService.deleteTodoById(todo.id);
+  }
+
+  get todos() {
+    return this.todoDataService.getAllTodos();
   }
 }
