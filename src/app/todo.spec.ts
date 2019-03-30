6a13f9fc6d94586b6e09337554e5ab00eb2908a5
pierrelambert41 <pierrelambert41@gmail.com>
2019-03-30 20:25:29 +0100

    ajout methode add & delete

--- src/app/todo.spec.ts
+++ src/app/todo.spec.ts
@@ -1,7 +1,17 @@
 import { Todo } from './todo';
+import { ExpectedConditions } from 'protractor';
 
 describe('Todo', () => {
   it('should create an instance', () => {
     expect(new Todo()).toBeTruthy();
+  });
+
+  it('Should accept values in the constructor', () => {
+    let todo = new Todo({
+      title: 'hello',
+      complete: true
+    });
+    expect(todo.title).toEqual('hello');
+    expect(todo.complete).toEqual(true);
   });
 });
